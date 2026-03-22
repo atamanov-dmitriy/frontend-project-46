fae32ec91398e7a9e671c1ee9800d2d869b9441b
Атаманов Дмитрий <atamanov.job@gmail.com>
2026-03-22 16:44:55 +0500

    Аргументы и опции

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
   .description("Compares two configuration files and shows a difference.")
-  .version("1.0.0");
+  .version("0.0.1")
+  .argument("<filepath1>", "path to first file")
+  .argument("<filepath2>", "path to second file")
+  .option("-f, --format [type]", "output format", "stylish");
 
 program.parse(process.argv);
